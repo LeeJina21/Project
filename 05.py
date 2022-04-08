5a7406348cc1f5367f14d76ebe73dc3f7cb889ae
LeeJina21 <vivid635@naver.com>
2022-04-08 10:36:51 +0900

    gg

---
+++ 05.py
@@ -0,0 +1,13 @@
+#코테 가장 큰 수
+def solution(numbers):
+
+    print(type(numbers))
+    print(type(numbers[0]))
+
+    numbers = list(map(str,numbers))
+
+    print(type(numbers))
+    print(type(numbers[0]))
+
+    numbers.sort(key=lambda x: x*3, reverse=True)
+    return str(int(''.join(numbers)))
---
+++ 06.py
@@ -0,0 +1,17 @@
+#코테 H index
+def solution(citations):
+    print(citations)
+    print(type(citations))
+    citations.sort(reverse=True)
+
+    print(citations)
+
+    for idx , citation in enumerate(citations):
+
+        print("idx:", idx)
+        print("citation:",citation)
+
+        if idx >= citation:
+            return idx
+
+    return len(citations)
